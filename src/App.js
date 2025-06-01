6f7471c505c8dc94f4a0df8027d75e86943eb82e
Sebbe <sebastian_hellman@live.se>
2025-06-01 21:48:04 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { Game } from './components/Game';
+import { Feedback } from './components/Feedback';
 
 function App() {
+
+  const [correctWord, setCorrectWord] = useState('');
+  const [scrambledWord, setScrambledWord] = useState('');
+  const [currentGuess, setCurrentGuess] = useState('')
+
+  const url = 'https://random-word-api.herokuapp.com/word?length=7';
+
+  const fetchWord = () => {
+   fetch(url).then(word => {return word.json()}).then(w => {setCorrectWord(w[0])}); 
+  }
+
+  useEffect(() => {
+   fetchWord()
+
+  }, [])
+
+  useEffect(() => {
+    
+   const handleType = (e) => {
+    if (/^[a-zA-Z]$/.test(e.key) && currentGuess.length < 7) {
+      setCurrentGuess(old => old + e.key)
+    }
+
+    if (e.keyCode ==  8) {
+      setCurrentGuess(old => old.substring(0, old.length - 1))
+    }
+
+    
+   }
+
+   window.addEventListener('keydown', handleType)
+   return () => {window.removeEventListener('keydown',handleType)}
+  }, [currentGuess])
+
+  useEffect(() => {
+    setScrambledWord(correctWord.split("").sort(() => 0.5 - Math.random()).join(""));
+    console.log(correctWord)
+  }, [correctWord])
+
+  useEffect(() => {
+    if (currentGuess === correctWord) {
+      console.log("You did it");
+      setTimeout(fetchWord, 3000)
+      setTimeout(() => setCurrentGuess(""), 3000)
+      document.getElementById("guess").value = '';
+      
+    }
+  }, [currentGuess])
+    
+  const handleGuessChange = (e) => {
+    const value = e.target.value;
+    setCurrentGuess(value);
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>
+        <h1>Bingo finder</h1>
+        <p>Can you find the 7 word from these tiles?</p>
+      </div>
+      <Game scrambledWord={scrambledWord} currentGuess={currentGuess} handleGuessChange={handleGuessChange}/>
+      <Feedback word={correctWord} guess={currentGuess}/>
+
     </div>
   );
 }
